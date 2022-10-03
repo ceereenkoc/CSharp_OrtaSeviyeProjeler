9362554b0ddbbc95081f83b857872eb525b23bc3
ceereenkoc <kocceereen@gmail.com>
2022-10-03 22:45:28 +0300

    Integer İkililerin Toplamı
    Ekrandan girilen n tane integer ikililerin toplamını alan, eğer sayılar birbirinden farklıysa toplamlarını ekrana yazdıran, sayılar aynıysa toplamının karesini ekrana yazdıran console uygulamasıdır.

---
+++ integerikililerintoplamı/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace integerikililerintoplamı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen çift bir sayı giriniz:");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+            int[] sayıListesi = new int[sayı];
+
+            if (sayı % 2 == 0)
+            {
+                Console.WriteLine($"{sayı} tane sayı giriniz.");
+                for (int i = 0; i < sayı; i++)
+                {
+                    Console.WriteLine("{0}. sayınız: ", i + 1);
+                    sayıListesi[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+
+                for (int i = 0; i < sayıListesi.Length; i += 2)
+                {
+                    if (sayıListesi[i] == sayıListesi[i + 1])
+                    {
+                        int toplam = sayıListesi[i] + sayıListesi[i + 1];
+                        Console.Write(" " + toplam * toplam + " ");
+                    }
+                    else
+                    {
+                        int toplam = sayıListesi[i] + sayıListesi[i + 1];
+                        Console.Write(" " + toplam + " ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\a Hatalı tuşlama! Lütfen çift sayı giriniz.");
+            }
+
+        }
+    }
+}
+
