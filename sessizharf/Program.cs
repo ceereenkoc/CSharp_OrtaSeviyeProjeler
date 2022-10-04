7ed06ad1fe113a40ddd2704a668e34b2963bea76
ceereenkoc <kocceereen@gmail.com>
2022-10-04 13:41:46 +0300

    Sessiz Harf
    Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasıdır.

---
+++ sessizharf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sessizharf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen aralarında boşluk bırakarak kelime giriniz:");
+            string[]  kelimeler = Console.ReadLine().Split();
+
+            foreach (var item in kelimeler)
+            {
+                bool değer = false;
+                for (int i = 1; i < item.Length; i++)
+                {
+                    if (sessiz(item[i]) == sessiz(item[i - 1]))
+                        değer = true;
+                    
+                }
+                Console.Write(değer + " ");
+            }
+
+            bool sessiz(char c)
+            {
+               return !"aeıioöuü".ToCharArray().Contains(c);             
+            }
+        }
+    }
+}
+
